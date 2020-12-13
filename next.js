95b8183f9d00dac5fab634f723ff2fe291ceb8d6
Cade Scroggins <git@cade.me>
2020-12-13 18:53:04 +0530

    fix withTranspileModules

--- next.js
+++ next.js
@@ -9,10 +9,7 @@ const cdkOutputsExist = fs.existsSync(cdkOutputsFile);
 const cdkOutputs = cdkOutputsExist ? require(cdkOutputsFile) : {};
 
 module.exports = withComposePlugins(
-  [
-    [withTranspileModules, ['a-uik', 'a-util']],
-    [withOptimizedImages, {}],
-  ],
+  [[withTranspileModules(['a-uik', 'a-util'])], [withOptimizedImages]],
   {
     devIndicators: { autoPrerender: false },
     env: cdkOutputs[Object.keys(cdkOutputs)[0]],
