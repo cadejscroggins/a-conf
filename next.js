672391295fcc6d9d89748089e5fd02899a8d34a2
Cade Scroggins <git@cade.me>
2020-12-13 19:15:05 +0530

    no optimized images

--- next.js
+++ next.js
@@ -1,7 +1,6 @@
 const appRootPath = require('app-root-path');
 const fs = require('fs');
 const withComposePlugins = require('next-compose-plugins');
-const withOptimizedImages = require('next-optimized-images');
 const withTranspileModules = require('next-transpile-modules');
 
 const cdkOutputsFile = `${appRootPath}/cdk.outputs.json`;
@@ -9,19 +8,9 @@ const cdkOutputsExist = fs.existsSync(cdkOutputsFile);
 const cdkOutputs = cdkOutputsExist ? require(cdkOutputsFile) : {};
 
 module.exports = withComposePlugins(
-  [[withTranspileModules(['a-uik', 'a-util'])], [withOptimizedImages]],
+  [[withTranspileModules(['a-uik', 'a-util'])]],
   {
     devIndicators: { autoPrerender: false },
     env: cdkOutputs[Object.keys(cdkOutputs)[0]],
-    webpack: (config) => {
-      const newConfig = config;
-
-      newConfig.module.rules = [
-        ...config.module.rules,
-        { test: /\.txt$/i, use: 'raw-loader' },
-      ];
-
-      return newConfig;
-    },
   }
 );
