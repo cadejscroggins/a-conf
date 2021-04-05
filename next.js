0b74ac2eef530ff742476811c8d25fec5be833cc
Cade Scroggins <git@cade.me>
2021-04-05 13:56:05 -0700

    add next bundle analyzer

--- next.js
+++ next.js
@@ -1,14 +1,22 @@
 const appRootPath = require('app-root-path');
 const fs = require('fs');
+
 const withComposePlugins = require('next-compose-plugins');
 const withTranspileModules = require('next-transpile-modules')(['a-uik']);
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const cdkOutputsFile = `${appRootPath}/cdk.outputs.json`;
 const cdkOutputsExist = fs.existsSync(cdkOutputsFile);
 const cdkOutputs = cdkOutputsExist ? require(cdkOutputsFile) : {};
 
-module.exports = withComposePlugins([withTranspileModules], {
-  devIndicators: { autoPrerender: false },
-  env: cdkOutputs[Object.keys(cdkOutputs)[0]],
-  future: { webpack5: true },
-});
+module.exports = withComposePlugins(
+  [withTranspileModules, withBundleAnalyzer],
+  {
+    devIndicators: { autoPrerender: false },
+    env: cdkOutputs[Object.keys(cdkOutputs)[0]],
+    future: { webpack5: true },
+  }
+);
